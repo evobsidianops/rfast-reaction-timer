ccac659d51a1023c6e72105f9b1c225b54a1d018
Conradgabe <gisuekebho5880@gmail.com>
2023-05-31 11:10:45 +0100

    Block child compoenent

---
+++ src/components/Block.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="block" @click="stopTimer" v-if="showBlock">
+        click me
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["delay"],
+    data() {
+        return {
+            timer: null,
+            reactionTime: 0,
+            showBlock: false
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            console.log("component Mounted")
+        this.showBlock = true
+        this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10);
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            this.$emit("end", this.reactionTime)
+            console.log(this.reactionTime)
+        }
+    }
+   
+}
+
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
